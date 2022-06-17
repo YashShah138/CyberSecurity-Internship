58ada4c8d306216b4bde7b9018b379c99fe578f9
Ubuntu <ubuntu@ip-172-31-81-230.ec2.internal>
2022-06-17 17:50:33 +0000

    Added Week 1 Requirement files

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo apt install pygame
+sudo apt install pandas
+sudo apt install matlab
+sudo apt install flask
+sudo apt install sqlalchemy
+sudo apt install NumPy
+sudo apt install matplotlib
+sudo apt install seaborn
+sudo apt install scikit-learn
+sudo apt install Requests
+sudo apt install NLTK
+sudo apt install urllib3
---
+++ remove.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt-get purge --auto-remove pandas
+sudo apt-get purge --auto-remove pygame
+sudo apt-get purge --auto-remove matlab
+sudo apt-get purge --auto-remove flask
+sudo apt-get purge --auto-remove NumPy
+sudo apt-get purge --auto-remove seaborn
